ae68f7ea7394000e48feca1fe9383623a5818d9e
cmorling <caspian.morling@gmail.com>
2020-05-26 10:41:10 +0200

    fixed error

--- typeracer.py
+++ typeracer.py
@@ -2,18 +2,22 @@ import re
 import sys
 import time
 from pynput.keyboard import Key, Controller
-
+import requests
 keyboard=Controller()
 
-with open(sys.argv[1], 'r') as file:
-	data=file.read().replace('\n', '')	
-	wordSorted = re.split('\",\"', data)
-	del wordSorted[0]
-	typertext = (max(wordSorted, key=len)).decode("unicode-escape").encode('ascii')
-	time.sleep(2)
-	print('')
-	for i in typertext:
-		keyboard.press(i)
-		keyboard.release(i)
-		print('printing ' + str(i))
-		time.sleep(.02)
+def typeText(input):
+	with open(input, 'r') as file:
+		data=file.read().replace('\n', '')	
+		wordSorted = re.split('\",\"', data)
+		del wordSorted[0]
+		phrase = max(wordSorted, key=len)
+		print(type(phrase.encode('utf-8')))
+		typertext = (max(wordSorted, key=len)).encode('utf-8').decode("unicode-escape").encode('ascii').decode('utf-8')
+		time.sleep(2)
+		print('')
+		for i in typertext:
+			keyboard.press(i)
+			keyboard.release(i)
+			print('printing ' + str(i))
+			time.sleep(.012)
+typeText(sys.argv[1])
